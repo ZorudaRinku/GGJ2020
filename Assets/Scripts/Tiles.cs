4197c68f0550e053b2ac445bfd8064c8edb486f2
Joseph Snedeker <Joseph.scott.snedeker@gmail.com>
2020-01-31 20:41:51 -0700

    Created tile system with ship and water tile tile maps, each tile map has a script with an enum that states what type it is

---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum tileType
+{
+    Ship, 
+    Water,
+    Turret,
+    Porthole
+}
+public class Tiles : MonoBehaviour
+{
+    [SerializeField]
+    tileType TileType;
+    
+
+    
+}
