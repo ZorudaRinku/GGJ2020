6100358f5df6bcec7dfb24bbc67390bef5e2b16e
Joseph Snedeker <Joseph.scott.snedeker@gmail.com>
2020-02-01 12:19:53 -0700

    started work on anchor

--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -8,50 +8,92 @@ public class TurretManager : MonoBehaviour
     GameObject turret;
     [SerializeField]
     GameObject cannonParent;
+    GameObject anchor;
     static public List<GameObject> cannons = new List<GameObject>();
     static public GameObject currentCannon;
     int currentCannonIndex;
+    bool turretMode;
+    float anchorMove = .01f;
+    
+    bool anchorMode;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(wait());
+        anchor = GameObject.FindWithTag("Anchor");
+        anchor.transform.position = new Vector3(transform.position.x, transform.position.y - 5, 0);
         
-        Debug.Log(cannons.Count);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1))
+        if (Input.GetKeyDown(KeyCode.KeypadPlus)) 
         {
-            if (currentCannon == cannons[0])
+
+        }
+        if (turretMode)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1))
             {
-                currentCannon = cannons[cannons.Count - 1];
-                currentCannonIndex = cannons.Count - 1;
+                if (currentCannon == cannons[0])
+                {
+                    currentCannon = cannons[cannons.Count - 1];
+                    currentCannonIndex = cannons.Count - 1;
+                }
+                else
+                {
+                    currentCannon = cannons[currentCannonIndex - 1];
+                }
             }
-            else
+            if (Input.GetKeyDown(KeyCode.Keypad3))
             {
-                currentCannon = cannons[currentCannonIndex - 1];
+                if (currentCannon == cannons[cannons.Count - 1])
+                {
+                    currentCannon = cannons[0];
+                    currentCannonIndex = 0;
+                }
+                else
+                {
+                    currentCannon = cannons[currentCannonIndex + 1];
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                Instantiate(turret, new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 0), transform.rotation);
             }
         }
-        if (Input.GetKeyDown(KeyCode.Keypad3))
+        else if (anchorMode)
         {
-            if (currentCannon == cannons[cannons.Count - 1])
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
-                currentCannon = cannons[0];
-                currentCannonIndex = 0;
+                anchor.transform.position = new Vector2(transform.position.x - anchorMove, transform.position.y);
+                
+
             }
-            else
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                currentCannon = cannons[currentCannonIndex + 1];
+                anchor.transform.position = new Vector2(transform.position.x + anchorMove, transform.position.y);
+                
             }
-        }
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            Instantiate(turret, new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 0), transform.rotation);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                anchor.transform.position = new Vector2(transform.position.x, transform.position.y + anchorMove);
+                
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                anchor.transform.position = new Vector2(transform.position.x, transform.position.y - anchorMove);
+
+                
+            }
+
+
         }
     }
+        
 
     IEnumerator wait()
     {
