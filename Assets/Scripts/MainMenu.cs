69db122d8bfcbc9d55203403607e54cd6d2f64d0
Zoruda <stronger6@gmail.com>
2020-01-31 22:37:18 -0700

    Main Menu + Options Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play ()
+    {
+        Debug.Log("Play Button Pressed");
+        EventSystem.current.SetSelectedGameObject(null);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options Button Pressed");
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Button Pressed");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void SetMusic(float value)
+    {
+        this.transform.Find("Music").GetComponent<AudioSource>().volume = value;
+    }
+
+    public void SetSound(float value)
+    {
+        this.transform.Find("Sound").GetComponent<AudioSource>().volume = value;
+    }
+}
