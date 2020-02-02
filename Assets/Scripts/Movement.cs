3cee1430fc20ae1dfd5afe106e1b709e08eb9796
RussAMR <44710713+RussAMR@users.noreply.github.com>
2020-02-02 14:42:37 -0700

    Repair

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -21,6 +21,7 @@ public class Movement : MonoBehaviour
     GameObject RepairTile;
     bool canBuild = true;
     bool canRepair = true;
+    bool repairing = false;
     [SerializeField]
     GameObject Building;
 
@@ -54,129 +55,143 @@ public class Movement : MonoBehaviour
         //    transform.position = new Vector2(transform.position.x - 1, transform.position.y);
         //}
 
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Physics2D.OverlapCircle(new Vector2(transform.position.x - 1, transform.position.y), 0.2f) != null)
             {
-                if (Physics2D.OverlapCircle(new Vector2(transform.position.x - 1, transform.position.y), 0.2f) != null)
+                transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (Physics2D.OverlapCircle(new Vector2(transform.position.x + 1, transform.position.y), 0.2f) != null)
+            {
+                transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + 1), 0.2f) != null)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 1), 0.2f) != null)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+            }
+        }
+        //GameObject tile = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), 0.2f).gameObject;
+
+        if (canBuild)
+        {
+
+            if (buildTimerLeft <= 0 && gameManager.coins >= 500)
+            {
+                if (Physics2D.OverlapCircle(new Vector2(transform.position.x - 1, transform.position.y), 0.2f) == null)
                 {
-                    transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+                    Instantiate(newTile, new Vector3(transform.position.x - 1, transform.position.y, 0), transform.rotation);
+                    canBuild = false;
+                    gameManager.coins -= 500;
                 }
 
+            }
+            if (buildTimerRight <= 0 && gameManager.coins >= 500)
+            {
+                if (Physics2D.OverlapCircle(new Vector2(transform.position.x + 1, transform.position.y), 0.2f) == null)
+                {
+                    Instantiate(newTile, new Vector3(transform.position.x + 1, transform.position.y, 0), transform.rotation);
+                    canBuild = false;
+                    gameManager.coins -= 500;
+                }
 
             }
-            if (Input.GetKeyDown(KeyCode.D))
+            if (buildTimerCannon <= 0 && gameManager.coins >= 1500)
             {
-                if (Physics2D.OverlapCircle(new Vector2(transform.position.x + 1, transform.position.y), 0.2f) != null)
-                {
-                    transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+                    canBuild = false;
+                    gameManager.coins -= 1500;
+                    StartCoroutine(Wait(3));
+            }
+        }
+        if(canRepair)
+        {
+            if (repairtimer <= 0)
+            {
+                if (currentTile.GetComponent<Health>().health == 1 && gameManager.coins >= 200)
+                {  
+                    currentTile.GetComponent<Health>().health++;
+                    repairtimer = 30;
+                    gameManager.coins -= 200;
+                    canBuild = false;
                 }
             }
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + 1), 0.2f) != null)
-                {
-                    transform.position = new Vector2(transform.position.x, transform.position.y + 1);
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 1), 0.2f) != null)
-                {
-                    transform.position = new Vector2(transform.position.x, transform.position.y - 1);
-                }
-            }
-            //GameObject tile = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), 0.2f).gameObject;
+        }
 
-            if (canBuild)
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            buildTimerLeft = 90;
+            buildTimerRight = 90;
+            buildTimerCannon = 380;
+            repairtimer = 30;
+            foreach (GameObject build in GameObject.FindGameObjectsWithTag("Build"))
             {
-
-                if (buildTimerLeft <= 0 && gameManager.coins >= 500)
-                {
-                    if (Physics2D.OverlapCircle(new Vector2(transform.position.x - 1, transform.position.y), 0.2f) == null)
-                    {
-                        Instantiate(newTile, new Vector3(transform.position.x - 1, transform.position.y, 0), transform.rotation);
-                        canBuild = false;
-                        gameManager.coins -= 500;
-                    }
-
-                }
-                if (buildTimerRight <= 0 && gameManager.coins >= 500)
-                {
-                    if (Physics2D.OverlapCircle(new Vector2(transform.position.x + 1, transform.position.y), 0.2f) == null)
-                    {
-                        Instantiate(newTile, new Vector3(transform.position.x + 1, transform.position.y, 0), transform.rotation);
-                        canBuild = false;
-                        gameManager.coins -= 500;
-                    }
-
-                }
-                if (buildTimerCannon <= 0 && gameManager.coins >= 1500)
-                {
-                        canBuild = false;
-                        gameManager.coins -= 1500;
-                        StartCoroutine(Wait(3));
-                }
+                Destroy(build);
             }
-            if(canRepair)
+            canBuild = true;
+            repairing = false;
+        }
+
+        if (buildTimerRight == 0 || buildTimerLeft == 0 || buildTimerCannon == 1 || repairtimer == 1)
+        {
+            foreach (GameObject build in GameObject.FindGameObjectsWithTag("Build"))
             {
-                if(Input.GetKeyUp(KeyCode.Space))
-                {
-                    if (currentTile.GetComponent<Health>().health == 1 && repairtimer <= 0)
-                    {
-                        currentTile.GetComponent<Health>().health += 1;
-                    }
-                }
+                Destroy(build);
             }
+        }
 
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                buildTimerLeft = 90;
-                buildTimerRight = 90;
-                buildTimerCannon = 380;
-                foreach (GameObject build in GameObject.FindGameObjectsWithTag("Build"))
-                {
-                    Destroy(build);
-                }
-                canBuild = true;
-            }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Space))
+        {
+            buildTimerLeft--;
+        }
 
-            if (buildTimerRight == 0 || buildTimerLeft == 0 || buildTimerCannon == 1)
-            {
-                foreach (GameObject build in GameObject.FindGameObjectsWithTag("Build"))
-                {
-                    Destroy(build);
-                }
-            }
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Space))
+        {
+            buildTimerRight--;
+        }
+        else if (Input.GetKey(KeyCode.Space) && currentTile.GetComponent<Health>().health == 2 && !repairing)
+        {
+            Debug.Log(buildTimerCannon);
+            buildTimerCannon--;
+        }
+        else if (Input.GetKey(KeyCode.Space) && currentTile.GetComponent<Health>().health == 1)
+        {
+            repairing = true;
+            Debug.Log(repairtimer);
+            repairtimer--;
+        }
 
-            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Space))
-            {
-                buildTimerLeft--;
-            }
+        if (buildTimerCannon == 379 && gameManager.coins >= 1500)
+        {
+            Instantiate(Building, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+        }
 
-            else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Space))
-            {
-                buildTimerRight--;
-            }
-            else if (Input.GetKey(KeyCode.Space))
-            {
-                Debug.Log(buildTimerCannon);
-                buildTimerCannon--;
-            }
+        if (buildTimerLeft == 89 && gameManager.coins >= 500)
+        {
+            Instantiate(Building, new Vector3(transform.position.x - 1, transform.position.y, 0), transform.rotation);
+        }
 
-            if (buildTimerCannon == 379 && gameManager.coins >= 1500)
-            {
-                Instantiate(Building, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
-            }
+        if (buildTimerRight == 89 && gameManager.coins >= 500)
+        {
+            Instantiate(Building, new Vector3(transform.position.x + 1, transform.position.y, 0), transform.rotation);
+        }
 
-            if (buildTimerLeft == 89 && gameManager.coins >= 500)
-            {
-                Instantiate(Building, new Vector3(transform.position.x - 1, transform.position.y, 0), transform.rotation);
-            }
-
-            if (buildTimerRight == 89 && gameManager.coins >= 500)
-            {
-                Instantiate(Building, new Vector3(transform.position.x + 1, transform.position.y, 0), transform.rotation);
-            }
+        if (repairtimer == 29 && gameManager.coins >= 200)
+        {
+            Instantiate(Building, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
         }
 
     }
@@ -187,9 +202,6 @@ public class Movement : MonoBehaviour
         buildTimerCannon = 380;
 
         yield return new WaitForSeconds(waitTime);
-
-
-
 
     }
 
@@ -213,18 +225,20 @@ public class Movement : MonoBehaviour
         if (other.tag == "Ship")
         {
             currentTile = other.transform.gameObject;
+            Debug.Log(currentTile);
+            Debug.Log(currentTile.GetComponent<Health>().health);
             if (currentTile.GetComponent<Health>().health != 2)
             {
                 canBuild = false;
 
             }
-            if (currentTile.GetComponent<Health>().health == 2)
+            if (currentTile.GetComponent<Health>().health == 1)
             {
-                canRepair = false;
+                canRepair = true;
             }
             else
             {
-                canRepair = true;
+                canRepair = false;
             }
         }
 
