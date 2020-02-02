d4da68436b807c3fea50adfbfe34e841c1d617f0
JosephSnedeker <44710728+JosephSnedeker@users.noreply.github.com>
2020-02-02 12:23:07 -0700

    balanced build times

---
+++ Assets/Scripts/GoldCOunt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GoldCOunt : MonoBehaviour
+{
+    TextMeshProUGUI gameTimerText;
+    int gameTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameTimer = 0;
+        gameTimerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer++;
+        gameTimerText.text = "Time: " + ((int)Time.time).ToString();
+    }
+}
