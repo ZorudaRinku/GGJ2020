e48e397f2f32534dcfd27c1ee5e756c672d4e24f
Logan Arakaki <larakaki@uccs.edu>
2020-02-01 19:02:03 -0700

    coins float

--- Assets/Scripts/coinController.cs
+++ Assets/Scripts/coinController.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class coinController : MonoBehaviour
 {
+    private Rigidbody2D rb2D;
+
+    [SerializeField] GameObject heartShip;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.y > heartShip.transform.position.y)
+        {
+            rb2D.gravityScale = 0.1f;
+        }
+        else
+        {
+            rb2D.gravityScale = -0.1f;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D anchorCollision)
