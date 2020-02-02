79751651281b90b009622c27687d7a6df0b3e0d3
Zoruda <stronger6@gmail.com>
2020-02-01 23:57:37 -0700

    Tutorial Prefab (incomplete)

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    TextMeshProUGUI TutorialText;
+    TextMeshProUGUI NextButton;
+
+    public void Start()
+    {
+        TutorialText = this.gameObject.transform.Find("Tutorial Pages").GetComponentInChildren<TextMeshProUGUI>();
+        NextButton = this.gameObject.transform.Find("Next").Find("Text").GetComponent<TextMeshProUGUI>();
+        TutorialText.text = TutorialTexts[page];
+        Time.timeScale = 0f;
+    }
+
+    int page = 0;
+    string[] TutorialTexts = new string[]{"Welcome to ShipWright!\n\nA co-op game!",
+        "One player controls the shipwright,\nthe other controls the cannons.\n\nThe shipwright has to repair the ship\nfrom the onslaught from the seas and skies.",
+        "Shipwright Controls:\n\nWASD to move\nStand still and hold spacebar to build a cannon\nHold spacebar + left or right to build in that direction", "Gunner Controls:\n\nEnter to fire\nLeft and right arrows to turn cannon\nNum 1 previous cannon/Num 3 next cannon", "Anchor Controls:\n\nNumpad + to swap to anchor (arrow keys)"};
+    
+    public void Next()
+    {
+        if (page + 1 == TutorialTexts.Length)
+        {
+            this.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if (page + 1 < TutorialTexts.Length){
+            page = page + 1;
+            TutorialText.text = TutorialTexts[page];
+        }
+        if (page + 1 == TutorialTexts.Length)
+        {
+            NextButton.text = "Done";
+        }
+    }
+
+    public void Previous()
+    {
+        if (page >= 0)
+        {
+            page = page - 1;
+            TutorialText.text = TutorialTexts[page];
+        }
+        if (page + 1 != TutorialTexts.Length)
+        {
+            NextButton.text = "Next";
+        }
+    }
+
+}
